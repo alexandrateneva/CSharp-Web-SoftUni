b1e3407950555325c8ada84339acf565b7bd4a7e
Alexandra Teneva <alexteneva@gmail.com>
2018-09-27 13:05:42 +0300

    Add Asynchronous Programming lab

---
+++ CSharp Web Development Basics/Asynchronous Programming/1.Even Numbers Thread/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace _1.Even_Numbers_Thread
+{
+    using System;
+    using System.Threading;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.Write("Enter start and end number separated by space: ");
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var start = int.Parse(input[0]);
+            var end = int.Parse(input[1]);
+
+            Thread thread = new Thread(() => PrintEvenNumbers(start, end));
+            thread.Start();
+            thread.Join();
+            Console.WriteLine("Thread finished work!");
+        }
+
+        private static void PrintEvenNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ CSharp Web Development Basics/Asynchronous Programming/2.Slice File/Startup.cs
@@ -0,0 +1,73 @@
+﻿namespace _2.Slice_File
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.Write("Enter video path: ");
+            var videoPath = Console.ReadLine();
+            Console.Write("Enter destination of the new folder: ");
+            var destination = Console.ReadLine();
+            Console.Write("Enter count of pieces: ");
+            var pieces = int.Parse(Console.ReadLine());
+
+            SliceAsync(videoPath, destination, pieces);
+
+            Console.WriteLine("Anything else?");
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void Slice(string sourceFile, string destinationPath, int parts)
+        {
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+
+            using (var source = new FileStream(sourceFile, FileMode.Open))
+            {
+                FileInfo fileInfo = new FileInfo(sourceFile);
+                long partLength = (int)Math.Ceiling((decimal)source.Length / parts);
+                long currentByte = 0;
+
+                for (int currentPart = 1; currentPart <= parts; currentPart++)
+                {
+                    string filePath = string.Format("{0}/Part-{1}{2}", destinationPath, currentPart, fileInfo.Extension);
+
+                    using (var destination = new FileStream(filePath, FileMode.Create))
+                    {
+                        var bufferLen = Math.Min(partLength, 4096);
+                        byte[] buffer = new byte[bufferLen];
+                        while (currentByte < partLength * currentPart)
+                        {
+                            int readBytesCount = source.Read(buffer, 0, buffer.Length);
+                            if (readBytesCount == 0)
+                            {
+                                break;
+                            }
+
+                            destination.Write(buffer, 0, readBytesCount);
+                            currentByte += readBytesCount;
+                        }
+                    }
+                }
+                Console.WriteLine("Slice complete.");
+            }
+        }
+
+        static void SliceAsync(string sourceFile, string destinationPath, int parts)
+        {
+            Task.Run(() =>
+            {
+                Slice(sourceFile, destinationPath, parts);
+            });
+        }
+    }
+}
