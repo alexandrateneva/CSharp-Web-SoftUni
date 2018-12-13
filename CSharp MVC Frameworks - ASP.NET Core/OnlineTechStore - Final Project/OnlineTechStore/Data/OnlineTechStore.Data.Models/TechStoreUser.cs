f3a8a2566c72a70c40c498c23c792d78733ce0cb
Alexandra Teneva <alexteneva@gmail.com>
2018-12-13 18:31:15 +0200

    Delete OnlineTechStore Project

--- CSharp MVC Frameworks - ASP.NET Core/OnlineTechStore - Final Project/OnlineTechStore/Data/OnlineTechStore.Data.Models/TechStoreUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineTechStore.Data.Models
-{
-    using Microsoft.AspNetCore.Identity;
-
-    // Add profile data for application users by adding properties to the TechStoreUser class
-    public class TechStoreUser : IdentityUser
-    {
-    }
-}
