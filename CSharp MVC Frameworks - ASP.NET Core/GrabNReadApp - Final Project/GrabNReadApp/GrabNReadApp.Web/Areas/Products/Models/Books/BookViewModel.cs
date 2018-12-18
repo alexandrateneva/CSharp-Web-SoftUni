eb6b27a2c5c4af3e4c98d2715aabaf150a83b637
Alexandra Teneva <alexteneva@gmail.com>
2018-12-18 15:33:14 +0200

    Create ViewModels for Genre and Book and change namespaces

---
+++ CSharp MVC Frameworks - ASP.NET Core/GrabNReadApp - Final Project/GrabNReadApp/GrabNReadApp.Web/Areas/Products/Models/Books/BookViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using GrabNReadApp.Data.Models.Enums;
+using GrabNReadApp.Data.Models.Products;
+using Microsoft.AspNetCore.Http;
+
+namespace GrabNReadApp.Web.Areas.Products.Models.Books
+{
+    public class BookViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+        public string Author { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        [Range(0.01, 1000000.00)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0.01, 1000000.00)]
+        [Display(Name = "Price per Day")]
+        public decimal PricePerDay { get; set; }
+
+        [Required]
+        [Range(1, Int32.MaxValue)]
+        [Display(Name = "Number of Pages")]
+        public int Pages { get; set; }
+
+        [Required]
+        [Display(Name = "Cover Type")]
+        public CoverType CoverType { get; set; }
+
+        [Required]
+        [StringLength(1000, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 20)]
+        public string Description { get; set; }
+        
+        [Required]
+        [DataType(DataType.Upload)]
+        [Display(Name = "Cover Image")]
+        public IFormFile CoverImageFile { get; set; }
+
+        [Url]
+        public string CoverImage { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public int GenreId { get; set; }
+    }
+}
---
+++ CSharp MVC Frameworks - ASP.NET Core/GrabNReadApp - Final Project/GrabNReadApp/GrabNReadApp.Web/Areas/Products/Models/Genres/GenreViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace GrabNReadApp.Web.Areas.Products.Models.Genres
+{
+    public class GenreViewModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Upload)]
+        [Display(Name = "Genre Image")]
+        public IFormFile ImageFile { get; set; }
+
+        [Url]
+        public string Image { get; set; }
+    }
+}
