ab1ce8c64cb4b578c5b610943f2b86819d206f79
Alexandra Teneva <alexteneva@gmail.com>
2019-01-02 19:56:31 +0200

    Add Unit Tests for BooksService and GenresService

---
+++ CSharp MVC Frameworks - ASP.NET Core/GrabNReadApp - Final Project/GrabNReadApp/GrabNReadApp.Tests/Data.Services/Products/BooksServiceTest.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Linq;
+using GrabNReadApp.Data;
+using GrabNReadApp.Data.Models.Enums;
+using GrabNReadApp.Data.Models.Products;
+using GrabNReadApp.Data.Services.Products;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace GrabNReadApp.Tests.Data.Services.Products
+{
+    public class BooksServiceTest
+    {
+        public BooksServiceTest()
+        {
+            TestInitializer.Initialize();
+        }
+
+        [Fact]
+        public async void CreateMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Books.AddRange(GetBooks());
+            db.SaveChanges();
+
+            var repository = new DbRepository<Book>(db);
+            var booksService = new BooksService(repository);
+
+            var newBook = new Book()
+            {
+                Title = "The Notebook",
+                Author = "Nicholas Sparks",
+                Price = 25.50m,
+                PricePerDay = 2.50m,
+                Description = "Example description for this book...",
+                Pages = 278,
+                GenreId = 1,
+                CoverType = CoverType.SoftCover
+            };
+
+            //Act
+            var enteredBook = await booksService.Create(newBook);
+            var booksCount = db.Books.Count();
+
+            //Assert
+            Assert.Equal(4, booksCount);
+            Assert.NotNull(enteredBook);
+            Assert.Same(newBook, enteredBook);
+        }
+
+        [Fact]
+        public void GetAllBooksMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Books.AddRange(GetBooks());
+            db.SaveChanges();
+
+            var repository = new DbRepository<Book>(db);
+            var booksService = new BooksService(repository);
+
+            //Act
+            var books = booksService.GetAllBooks().ToList();
+            var firstBookInDb = db.Books.FirstOrDefault();
+
+            //Assert
+            Assert.Equal(3, books.Count);
+            Assert.Equal(firstBookInDb?.Id, books.LastOrDefault()?.Id);  //ORDER BY DESC
+        }
+
+        [Fact]
+        public async void EditMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Books.AddRange(GetBooks());
+            var newBook = new Book()
+            {
+                Title = "The Notebook",
+                Author = "Nicholas Sparks",
+                Price = 25.50m,
+                PricePerDay = 2.50m,
+                Description = "Example description for this book...",
+                Pages = 278,
+                GenreId = 1,
+                CoverType = CoverType.SoftCover
+            };
+            db.Books.Add(newBook);
+            db.SaveChanges();
+
+            var repository = new DbRepository<Book>(db);
+            var booksService = new BooksService(repository);
+
+            //Act
+            newBook.Price = 35.00m;
+            newBook.PricePerDay = 3.00m;
+
+            var enteredBook = await booksService.Edit(newBook);
+            var booksCount = db.Books.Count();
+
+            //Assert
+            Assert.Equal(4, booksCount);
+            Assert.NotNull(enteredBook);
+            Assert.Same(newBook, enteredBook);
+        }
+
+        [Fact]
+        public void DeleteMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Books.AddRange(GetBooks());
+            var newBook = new Book()
+            {
+                Id = 167282,
+                Title = "The Notebook",
+                Author = "Nicholas Sparks",
+                Price = 25.50m,
+                PricePerDay = 2.50m,
+                Description = "Example description for this book...",
+                Pages = 278,
+                GenreId = 1,
+                CoverType = CoverType.SoftCover
+            };
+            db.Books.Add(newBook);
+            db.SaveChanges();
+
+            var repository = new DbRepository<Book>(db);
+            var booksService = new BooksService(repository);
+
+            //Act
+            var isDeletedRealBook = booksService.Delete(167282);
+            var isDeletedNotExistingBook = booksService.Delete(167282);
+            var booksCount = db.Books.Count();
+
+            //Assert
+            Assert.True(isDeletedRealBook);
+            Assert.False(isDeletedNotExistingBook
+);
+            Assert.Equal(3, booksCount);
+        }
+
+        [Fact]
+        public async void GetByIdMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Books.AddRange(GetBooks());
+            var newBook = new Book()
+            {
+                Id = 167282,
+                Title = "The Notebook",
+                Author = "Nicholas Sparks",
+                Price = 25.50m,
+                PricePerDay = 2.50m,
+                Description = "Example description for this book...",
+                Pages = 278,
+                GenreId = 1,
+                CoverType = CoverType.SoftCover
+            };
+            db.Books.Add(newBook);
+            db.SaveChanges();
+
+            var repository = new DbRepository<Book>(db);
+            var booksService = new BooksService(repository);
+
+            //Act
+            var book = await booksService.GetBookById(167282);
+            var notExistingBook = await booksService.GetBookById(62891);
+
+            //Assert
+            Assert.NotNull(book);
+            Assert.Null(notExistingBook);
+            Assert.Same(newBook, book);
+        }
+
+        [Fact]
+        public void GetBooksByTitleMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Books.AddRange(GetBooks());
+            db.SaveChanges();
+
+            var repository = new DbRepository<Book>(db);
+            var booksService = new BooksService(repository);
+
+            //Act
+            var oneBookWithThisTitle = booksService.GetBooksByTitle(" THE booK of wHy ").ToList();
+            var manyBookWithThisTitle = booksService.GetBooksByTitle(" ThE ").ToList();
+            var noBookWithThisTitle = booksService.GetBooksByTitle(" TeSt ").ToList();
+
+            //Assert
+            Assert.Single(oneBookWithThisTitle);
+            Assert.Equal(2, manyBookWithThisTitle.Count);
+            Assert.Empty(noBookWithThisTitle);
+        }
+
+        private Book[] GetBooks()
+        {
+            return new Book[]
+            {
+               new Book()
+               {
+                   Title = "The Book Of Why",
+                   Author = "Judea Pearl",
+                   Price = 25.50m,
+                   PricePerDay = 2.50m,
+                   Description = "Example description for first book...",
+                   Pages = 360,
+                   GenreId = 1,
+                   CoverType = CoverType.HardCover,
+                   ReleaseDate = DateTime.UtcNow
+               },
+               new Book()
+               {
+                   Title = "Taking Medicine",
+                   Author = "Lyn Miller",
+                   Price = 50.00m,
+                   PricePerDay = 3.00m,
+                   Description = "Example description for second book...",
+                   Pages = 520,
+                   GenreId = 2,
+                   CoverType = CoverType.HardCover,
+                   ReleaseDate = DateTime.UtcNow.AddDays(5)
+               },
+               new Book()
+               {
+                   Title = "The jungle crew",
+                   Author = "Emma Scott",
+                   Price = 17.00m,
+                   PricePerDay = 1.50m,
+                   Description = "Example description for third book...",
+                   Pages = 66,
+                   GenreId = 3,
+                   CoverType = CoverType.SoftCover,
+                   ReleaseDate = DateTime.UtcNow.AddDays(3)
+               },
+            };
+        }
+
+        private GrabNReadAppContext GetDatabase()
+        {
+            var dbOptions = new DbContextOptionsBuilder<GrabNReadAppContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new GrabNReadAppContext(dbOptions);
+        }
+    }
+}
---
+++ CSharp MVC Frameworks - ASP.NET Core/GrabNReadApp - Final Project/GrabNReadApp/GrabNReadApp.Tests/Data.Services/Products/GenresServiceTest.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Linq;
+using GrabNReadApp.Data;
+using GrabNReadApp.Data.Models.Products;
+using GrabNReadApp.Data.Services.Products;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace GrabNReadApp.Tests.Data.Services.Products
+{
+    public class GenresServiceTest
+    {
+        public GenresServiceTest()
+        {
+            TestInitializer.Initialize();
+        }
+
+        [Fact]
+        public async void CreateMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Genre.AddRange(GetGenres());
+            db.SaveChanges();
+
+            var repository = new DbRepository<Genre>(db);
+            var genresService = new GenresService(repository);
+
+            var newGenre = new Genre()
+            {
+                Name = "Mystery"
+            };
+
+            //Act
+            var enteredGenre = await genresService.Create(newGenre);
+            var genresCount = db.Genre.Count();
+
+            //Assert
+            Assert.Equal(4, genresCount);
+            Assert.NotNull(enteredGenre);
+            Assert.Same(newGenre, enteredGenre);
+        }
+
+        [Fact]
+        public async void EditMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Genre.AddRange(GetGenres());
+
+            var newGenre = new Genre()
+            {
+                Name = "Mystery"
+            };
+            db.Genre.Add(newGenre);
+            db.SaveChanges();
+
+            var repository = new DbRepository<Genre>(db);
+            var genresService = new GenresService(repository);
+            
+            //Act
+            newGenre.Name = "Mystery & Thriller";
+            var changedGenre = await genresService.Edit(newGenre);
+            var genresCount = db.Genre.Count();
+
+            //Assert
+            Assert.Equal(4, genresCount);
+            Assert.NotNull(changedGenre);
+            Assert.Same(newGenre, changedGenre);
+        }
+
+        [Fact]
+        public void DeleteMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Genre.AddRange(GetGenres());
+
+            var newGenre = new Genre()
+            {
+                Id = 26818,
+                Name = "Mystery"
+            };
+            db.Genre.Add(newGenre);
+            db.SaveChanges();
+
+            var repository = new DbRepository<Genre>(db);
+            var genresService = new GenresService(repository);
+
+            //Act
+            var isDeletedRealGenre = genresService.Delete(26818);
+            var isDeletedNotExistingGenre = genresService.Delete(26818);
+            var genresCount = db.Genre.Count();
+
+            //Assert
+            Assert.Equal(3, genresCount);
+            Assert.True(isDeletedRealGenre);
+            Assert.False(isDeletedNotExistingGenre);
+        }
+
+        [Fact]
+        public async void GetGenreByIdMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Genre.AddRange(GetGenres());
+
+            var newGenre = new Genre()
+            {
+                Id = 82901,
+                Name = "Mystery"
+            };
+            db.Genre.Add(newGenre);
+            db.SaveChanges();
+
+            var repository = new DbRepository<Genre>(db);
+            var genresService = new GenresService(repository);
+
+            //Act
+            var genre = await genresService.GetGenreById(82901);
+            var notExistingGenre = await genresService.GetGenreById(71939);
+
+            //Assert
+            Assert.Null(notExistingGenre);
+            Assert.NotNull(genre);
+            Assert.Same(newGenre, genre);
+        }
+
+        [Fact]
+        public void GetAllGenresMethodHaveToWorkCorrectly()
+        {
+            //Arrange
+            var db = GetDatabase();
+            db.Genre.AddRange(GetGenres());
+            db.SaveChanges();
+
+            var repository = new DbRepository<Genre>(db);
+            var genresService = new GenresService(repository);
+
+            //Act
+            var genres = genresService.GetAllGenres().ToList().Count;
+
+            //Assert
+            Assert.Equal(3, genres);
+        }
+
+        private Genre[] GetGenres()
+        {
+            return new Genre[]
+            {
+                new Genre()
+                {
+                    Name = "Romance"
+                },
+                new Genre()
+                {
+                    Name = "Science"
+                },
+                new Genre()
+                {
+                    Name = "Fantasy"
+                }
+            };
+        }
+
+        private GrabNReadAppContext GetDatabase()
+        {
+            var dbOptions = new DbContextOptionsBuilder<GrabNReadAppContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new GrabNReadAppContext(dbOptions);
+        }
+    }
+}
